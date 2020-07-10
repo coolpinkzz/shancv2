f66010bd7653fe20fa7329dfb8aa0c4c8e1ca0a3
coolpinkzz <pratik.1rn17ee029@gmail.com>
2020-07-10 14:36:59 +0530

    font changed

---
+++ src/@reflexjs/gatsby-theme-core/theme.js
@@ -0,0 +1,13 @@
+export default {
+    colors: {
+      text: "#000",
+      background: "#fff",
+      primary: "#07c",
+      secondary: "#f8c",
+    },
+    fonts: {
+      body: "Sofia Pro, serif",
+      heading: "Alegreya", "serif",
+      monospace: "Menlo, monospace",
+    },
+  }
