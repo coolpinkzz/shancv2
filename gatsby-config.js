a4fd9d8516823e226061d9a95591c3aab6e4889c
coolpinkzz <pratik.1rn17ee029@gmail.com>
2020-07-10 10:25:52 +0530

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ require(`dotenv`).config()
 
 module.exports = {
   siteMetadata: {
-    title: "Reflex",
+    title: "Shanc",
     description: "Starter for Reflex.",
     siteUrl: process.env.SITE_URL || "http://localhost:8000",
   },
