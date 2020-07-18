941d0a0d6ec69ad692aa84b12c0f9e8c4b0d4e34
coolpinkzz <pratik.1rn17ee029@gmail.com>
2020-07-18 22:11:32 +0530

    footer added

---
+++ src/@reflexjs/gatsby-theme-base/theme.js
@@ -0,0 +1,14 @@
+import base from "@reflexjs/preset-base";
+
+export default {
+  preset: base,
+  // Overrides.
+  colors: {
+    primary: `#09f`,
+  },
+  fonts: {
+    body: "'Sofia Pro'",
+    heading: "'Alegreya', serif",
+    monospace: "Menlo, monospace",
+  },
+};
--- src/gatsby-theme-base/theme.js
+++
@@ -1,8 +0,0 @@
-export default {
-  colors: {
-    text: "#000",
-    background: "#fff"
-    primary: "red",
-  },
-  fontSizes: [14, 16, 18, 20, 22, 36, 56, 72],
-}
