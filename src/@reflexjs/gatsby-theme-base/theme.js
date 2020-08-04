7a5eb51e174fd9bb1ab497fc2bebd7c1c063c8ea
coolpinkzz <pratik.1rn17ee029@gmail.com>
2020-08-04 13:20:23 +0530

    html.js added

--- src/@reflexjs/gatsby-theme-base/theme.js
+++ src/@reflexjs/gatsby-theme-base/theme.js
@@ -5,6 +5,7 @@ import "typeface-noto-sans-sc";
 export default {
   preset: base,
   // Overrides.
+
   colors: {
     primary: `#09f`,
   },
@@ -12,16 +13,5 @@ export default {
     body: "noto sans sc",
     heading: "'Alegreya', serif",
     monospace: "Menlo, monospace",
-  },
-
-  HW_badge_cont: {
-    position: "absolute",
-  },
-  HW_badge: {
-    background: "#5ab987",
-  },
-  HW_config: {
-    selector: "H1", // CSS selector where to inject the badge
-    account: "JlR9Gx",
   },
 };
