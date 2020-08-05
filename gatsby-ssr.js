93a002227157051099b0f48a385628af8442c343
coolpinkzz <52915014+coolpinkzz@users.noreply.github.com>
2020-08-06 00:36:47 +0530

    Revert "Headway"

--- gatsby-ssr.js
+++
@@ -1,7 +0,0 @@
-import * as React from "react";
-
-export const onRenderBody = ({ setPostBodyComponents }) => {
-  setPostBodyComponents([
-    <script key="headway-widget" src="https://cdn.headwayapp.co/widget.js" />,
-  ]);
-};
